a0fb32421b7b2bb938f7c778d53eb54f14b0f350
J-HyeonSeo <47245112+J-HyeonSeo@users.noreply.github.com>
2023-05-31 13:43:30 +0900

    --수행 목록-- - plan테이블 추가 - stocksEntity 필드값 수정.(+ Lot추가) - 가이드라인 ppt파일 첨부

--- src/main/java/com/jhsfully/inventoryManagement/model/planEntity.java
+++ src/main/java/com/jhsfully/inventoryManagement/model/planEntity.java
@@ -1,8 +1,6 @@
 package com.jhsfully.inventoryManagement.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,15 +10,18 @@ import java.time.LocalDate;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
-@Entity(name = "stocks")
-public class stocksEntity {
+@Builder
+@Entity(name = "plan")
+public class planEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private long pid;
+    private LocalDate due;
+    private String destination;
     private double amount;
-    private LocalDate lot;
 
 }
